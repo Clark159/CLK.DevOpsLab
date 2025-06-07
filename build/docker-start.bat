326ed75942af17bcd7157b803641f39730209e09
Clark <clark159@hotmail.com>
2025-06-07 22:58:52 +0800

    xxxxx

--- build/docker-start.bat
+++ build/docker-start.bat
@@ -1,28 +1,45 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
+
+:: ===== variables =====
+set enableBitbucket=true
+set enableBamboo=false
+set enableJFrog=true
 
 :: ===== bitbucket =====
-if not exist "C:\DevOps\Bitbucket\data" (
+if /i "!enableBitbucket!"=="true" (
+  if not exist "C:\DevOps\Bitbucket\data" (
     mkdir "C:\DevOps\Bitbucket\data"
+  )
+  docker-compose --project-name devops up -d --build bitbucket
+  echo.
+  echo.
 )
-docker-compose --project-name devops up -d --build bitbucket
 
 :: ===== bamboo =====
-if not exist "C:\DevOps\Bamboo\data" (
+if /i "!enableBamboo!"=="true" (
+  if not exist "C:\DevOps\Bamboo\data" (
     mkdir "C:\DevOps\Bamboo\data"
+  )
+  docker-compose --project-name devops up -d --build bamboo
+  echo.
+  echo.
 )
-docker-compose --project-name devops up -d --build bamboo
 
 :: ===== jfrog =====
-if not exist "C:\DevOps\JFrog\data" (
+if /i "!enableJFrog!"=="true" (
+  if not exist "C:\DevOps\JFrog\data" (
     mkdir "C:\DevOps\JFrog\data"
+  )
+  docker-compose --project-name devops up -d --build jfrog
+  echo.
+  echo.
 )
-docker-compose --project-name devops up -d --build jfrog
 
 :: ===== End =====
-echo Bitbucket started: http://localhost:7990
-echo Bamboo started: http://localhost:8085
-echo JFrog started: http://localhost:8081
+if /i "!enableBitbucket!"=="true" echo Bitbucket: http://localhost:7990
+if /i "!enableBamboo!"=="true"    echo Bamboo:    http://localhost:8085
+if /i "!enableJFrog!"=="true"     echo JFrog:     http://localhost:8081
+echo.
+echo.
 pause
-echo.
-echo.
