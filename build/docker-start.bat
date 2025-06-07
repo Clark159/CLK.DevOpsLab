a961534e08478ccea0dbe84e6ff0aa3e856c04ce
Clark <clark159@hotmail.com>
2025-06-07 22:48:58 +0800

    x

--- build/docker-start.bat
+++ build/docker-start.bat
@@ -5,26 +5,24 @@ setlocal
 if not exist "C:\DevOps\Bitbucket\data" (
     mkdir "C:\DevOps\Bitbucket\data"
 )
+docker-compose --project-name devops up -d --build bitbucket
 
 :: ===== bamboo =====
 if not exist "C:\DevOps\Bamboo\data" (
     mkdir "C:\DevOps\Bamboo\data"
 )
+docker-compose --project-name devops up -d --build bamboo
 
 :: ===== jfrog =====
 if not exist "C:\DevOps\JFrog\data" (
     mkdir "C:\DevOps\JFrog\data"
 )
+docker-compose --project-name devops up -d --build jfrog
 
-:: ===== build =====
-docker-compose --project-name devops up -d --build jfrog bitbucket bamboo
+:: ===== End =====
 echo Bitbucket started: http://localhost:7990
 echo Bamboo started: http://localhost:8085
 echo JFrog started: http://localhost:8081
-echo.
-echo.
-
-:: ===== End =====
 pause
 echo.
 echo.
